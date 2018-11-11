36bb519981de2a94db5a31f9332e33b51aad15a0
Radu Crisan <radu.crisan@gmail.com>
2018-11-11 14:34:33 +0200

    updates

--- app.go
+++ app.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"log"
+	"math/rand"
 	"net"
+	"strconv"
+	"time"
 
 	"github.com/raducrisan1/microservice-stockinfo/stockinfo"
 	"google.golang.org/grpc"
@@ -26,8 +29,27 @@ func main() {
 }
 
 func (s *server) StockInfo(context.Context, *stockinfo.StockInfoRequest) (*stockinfo.StockInfoResponse, error) {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	rsp := stockinfo.StockInfoResponse{
-		Stockname: "NVDA",
-	}
+		Stockname:  "NVDA",
+		CciData:    randInt(5, -100, 100),
+		RsiData:    randInt(5, 0, 100),
+		MacdData:   randInt(5, -30, 30),
+		VolumeData: randInt(5, 5000, 50000)}
+
 	return &rsp, nil
+}
+
+func randInt(size int, min int, max int) []*stockinfo.Indicator {
+	result := make([]*stockinfo.Indicator, size)
+	timeRef, _ := time.Parse(time.RFC3339, "2018-11-11 09:30Z")
+	for i := range result {
+		res := new(stockinfo.Indicator)
+		res.Value = float32(min + rand.Intn(max-min))
+		d, _ := time.ParseDuration(strconv.Itoa(i+1) + "m")
+		res.Timestamp = timeRef.Add(d).Unix()
+		result[i] = res
+	}
+	return result
 }
